74a27f38f9d2583f8f88f670baf4136609a19599
rev1z <a.bektepov@gmail.com>
2018-04-18 14:11:33 +0300

    Update list.sql

--- list.sql
+++ list.sql
@@ -1,6 +1,16 @@
--- get duplucate rows
+-- Get duplucate rows
 
 SELECT id, column, count(*)
 FROM target_table
 GROUP BY id, column
 HAVING count(*) > 1
+
+
+-- Faster replacement for limit-offset(limit-offset makes fullscan)
+-- useful for extraction of large datasets from postgresql
+
+SELECT id, column1, column2, column3
+FROM table_name
+WHERE id > 1 and id <= 1 + step --here you have to find "step" programmatically 
+
+
